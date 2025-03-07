960d16fe5993699c9086425dfc5d7c3875b0ad93
ximenatiscareno08 <144255237+ximenatiscareno08@users.noreply.github.com>
2025-03-06 18:55:54 -0600

    octavo commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build' // Cambia la carpeta de salida a 'build'
-  }
-})
+    outDir: 'build' ,// Cambia la carpeta de salida a 'build'
+  },
+  server:{
+  port: 3000,
+},
+});
